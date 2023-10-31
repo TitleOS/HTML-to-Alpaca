29a709a68b56ada481ec2e3b0d1a32032610bca2
TitleOS <titleos@titleos.dev>
2023-10-31 18:01:32 -0500

    Added Eval arg that splits the created dataset in half to train and eval. Removed slow prints.

--- generate_alpaca.py
+++ generate_alpaca.py
@@ -7,6 +7,7 @@ parser.add_argument('--apikey', type=str, help='Your OpenAI API Key')
 parser.add_argument('--txt', type=str, help='Set Path to HTML processed text files')
 parser.add_argument('--output', type=str, help='Set the output file name for the alpaca dataset.')
 parser.add_argument('--num_questions', type=int, help='Set the number of questions to generate.')
+parser.add_argument('--eval', action='store_true', help='Set to split the dataset into train and test sets.')
 args = parser.parse_args()
 
 if args.apikey:
@@ -33,24 +34,28 @@ if args.num_questions:
 else:
     NUM_QUESTIONS = 10
     print("No number of questions provided, defaulting to 10")
+if args.eval:
+    EVAL = True
+    print(f"Splitting dataset into train and test sets.")
+else:
+    EVAL = False
+    print("Not splitting dataset into train and test sets.")
 
 # Initialize OpenAI API
 openai.api_key = api_key
 
 def generate_questions(text, num_questions=NUM_QUESTIONS):
-    prompt = f"Based on the following text generate {num_questions} instructions with their responses:\n\n{text}\n. Please include the instruction prefix and response suffix. Do not include numbers or count. For example, Instruction: Obama as born in 1961 in Hawaii.\n When was Obama born? Response: 1961\n"
-    print("Prompt: " + prompt)
+    prompt = f"Based on the following text generate {num_questions} instructions with their responses:\n\n{text}\n. Please include the instruction prefix and response suffix. Do not include numbers or count. Ignore irrelevant content from the text, and instead focus on providing high quality, dense, yet concise questions and responses that summarize the majority of the text. For example, Instruction: Former President Barack Obama as born in 1961 in Hawaii.\n When was Obama born? Response: 1961\n"
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         messages=[{"role": "user", "content": prompt}],
         max_tokens=2048
     )
-    print(response) 
     ir_pairs = response.choices[0].message.content.strip().split('\n\n')
     
     # Only keep pairs that can be split into an instruction and a response
     valid_pairs = [ir for ir in ir_pairs if len(ir.split('\n')) == 2]
-    
+    print([{"instruction": ir.split('\n')[0], "response": ir.split('\n')[1]} for ir in valid_pairs]) 
     return [{"instruction": ir.split('\n')[0], "response": ir.split('\n')[1]} for ir in valid_pairs]
 
 def process_text_files(directory_path):
@@ -72,9 +77,34 @@ def process_text_files(directory_path):
                     
                     
     # Output in modified Alpaca format
+    import json
+    if(EVAL):
+        alpaca_eval = alpaca_data[:len(alpaca_data)//2]
+        alpaca_train = alpaca_data[len(alpaca_data)//2:]
+
+        alpaca_eval_output = OUTPUT_PATH.split(".")[0] + "_eval.json"
+        alpaca_train_output = OUTPUT_PATH.split(".")[0] + "_train.json"
+
+
+        with open(alpaca_eval_output, "w", encoding="utf-8") as outfile:
+            json.dump(alpaca_eval, outfile, ensure_ascii=False, indent=4)
+            print("Wrote " + str(len(alpaca_eval)) + " pairs to " + alpaca_eval_output)
+
+        with open(alpaca_train_output, "w", encoding="utf-8") as outfile:
+            json.dump(alpaca_train, outfile, ensure_ascii=False, indent=4)
+            print("Wrote " + str(len(alpaca_train)) + " pairs to " + alpaca_train_output)
+
+    else:
+        with open(alpaca_data, "w", encoding="utf-8") as outfile:
+            json.dump(alpaca_data, outfile, ensure_ascii=False, indent=4)
+            print("Wrote " + str(len(alpaca_data)) + " pairs to " + alpaca_data)
+
+        
+
     with open(OUTPUT_PATH, "w", encoding="utf-8") as outfile:
         import json
         json.dump(alpaca_data, outfile, ensure_ascii=False, indent=4)
+        print("Wrote " + str(len(alpaca_data)) + " pairs to " + OUTPUT_PATH)
 
 if __name__ == "__main__":
     process_text_files(text_files)
